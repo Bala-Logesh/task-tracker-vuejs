36ce6fab709f882988e2038a53ba258e5e558b07
Bala Logesh S <balassmp@gmail.com>
2025-01-22 16:09:50 -0500

    Created Base Components

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <button :style="{ backgroundColor: color }" class="btn" @click="onClick()">
+        {{ title }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: "Button",
+    props: {
+        title: String,
+        color: String
+    },
+    methods: {
+        onClick() {
+            console.log("Button Clicked for " + this.title)
+        }
+    }
+}
+</script>
+
+<style scoped></style>
